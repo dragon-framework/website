10d52c0bba840f8d3385d1a04a97492652fe2eb3
Arnaud BODEL <contact@osw3.net>
2020-03-24 16:03:39 +0100

    -

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// if (!file_exists('../vendor/autoload.php')){
+// 	echo '<p>Vous devez installer les dépendances du projet avec la commande <code>composer install</code>. En effet, ceux-ci ne sont pas versionnés.</p>';
+// 	die();
+// }
+require '../vendor/autoload.php';
+
+// $app = new Odyssey\App();
+// $app->run();
+
+echo "Hello Dragon !";
