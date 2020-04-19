b21c210bd65810a40e5b18d401de45edc625bf20
Arnaud BODEL <contact@osw3.net>
2020-04-19 05:05:08 +0200

    -

--- config/security.php
+++ config/security.php
@@ -2,4 +2,8 @@
     'strategy' => "password",
     'token_expiration' => 900,
     'registration_allowed' => true,
+    // 'authentication_on_registration' => true,
+    // 'activation' => false,
+
+    'redirect_on_login' => "homepage",
 ];
--- src/Controllers/FrontOffice/DefaultController.php
+++ src/Controllers/FrontOffice/DefaultController.php
@@ -7,6 +7,11 @@ class DefaultController extends AbstractController
 {
     public function index()
     {
+        // dump( $this->user() );
+
+        // dump( $this->isAnonymous() );
+        // dump( $this->isAuthenticated() );
+
         // dump( $this->generateUrl('books:create') );
         // dump( $this->generateUrl('books:create', [], true) );
         
